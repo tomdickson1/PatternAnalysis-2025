2b0545b2fa1e0c54a4540ab7219289e81e4a0ef0
Tom Dickson <tomdickson17@gmail.com>
2025-10-30 09:44:52 +1000

    add function comments and argparse help descriptions to train.py

--- recognition/s4698845_improved_unet_pelvis/train.py
+++ recognition/s4698845_improved_unet_pelvis/train.py
@@ -1,3 +1,9 @@
+"""
+Functions to facilitate training, validating and saving the 3D Improved UNet
+model.
+
+@author Tom Dickson
+"""
 
 import torch
 from torch.utils.tensorboard import SummaryWriter
@@ -10,9 +16,24 @@ from dataset import make_dataloaders, TRAIN_IDS, VAL_IDS, TEST_IDS
 import torchio as tio
 import argparse
 
-def train(net: AbstractNetwork, optimiser: optim.Optimizer, train_loader, val_loader, epochs, device, val_check_factor=2, time_limit=0):
-    """Perform training with given epochs and time limit.
-    If specified, restart training from previous run.
+# number of classes in the dataset
+N_CLASSES = 6
+INITIAL_UNET_CHANNELS = 16
+UNET_DEPTH = 4
+
+def train(net: AbstractNetwork, optimiser: optim.Optimizer, train_loader, val_loader, epochs, device, val_check_factor=2):
+    """Train the given network with the given optimiser, using data from the 
+    given training loader. 
+
+    Args:
+        net (AbstractNetwork): network to train
+        optimiser (optim.Optimizer): optimiser to use
+        train_loader (tio.SubjectsLoader): dataloader for training data
+        val_loader (tio.SubjectsLoader): dataloader for validation data
+        epochs (int): number of epochs to train for
+        device (str): pytorch device to use
+        val_check_factor (int, optional): calculate validation statistics only
+            every `val_check_factor` epochs. Useful on slow machines. Defaults to 2.
     """
     timestamp = str(time.strftime("%Y%m%d-%H%M%S"))
     writer = SummaryWriter(f'runs/{timestamp}')
@@ -62,6 +83,18 @@ def train(net: AbstractNetwork, optimiser: optim.Optimizer, train_loader, val_lo
     torch.save(optimiser.state_dict(), f"models/{timestamp}.optim")
 
 def test(net : AbstractNetwork, loader, device, writer: SummaryWriter=None, epoch=None):
+    """Test the given network with data from the given SubjectsLoader. If using
+    inside a training loop, can supply a tensorboard summary writer and epoch
+    number so data can be saved.
+
+    Args:
+        net (AbstractNetwork): network to test
+        loader (tio.SubjectsLoader): source of testing data
+        device (str): pytorch device to use
+        writer (SummaryWriter, optional): Tensorboard writer to use for saving data.
+            If not provided, data is not saved
+        epoch (int, optional): epoch number to use when writing to Tensorboard. Defaults to None.
+    """
     net.eval()
     with torch.no_grad():
         running_xy = torch.zeros((1, net.n_classes), device=device)
@@ -99,16 +132,25 @@ def test(net : AbstractNetwork, loader, device, writer: SummaryWriter=None, epoc
 
 
 def main():
+    """Function to execute functions according to user
+    specified arguments.
+    """
     parser = argparse.ArgumentParser(
                     prog='train.py',
                     description='trains the 3D Improved UNet Model',
                     )
-    parser.add_argument('--test', action='store_true')
-    parser.add_argument('--compile', action='store_true')
-    parser.add_argument('--prev')
-    parser.add_argument('--epochs')
-    parser.add_argument('--data')
-    parser.add_argument('--batch', default=1)
+    parser.add_argument('--test',
+                        action='store_true',
+                        help="test the model on the test set (requires specifying --prev)")
+    parser.add_argument('--compile',
+                        action='store_true',
+                        help="JIT compile the model for faster performance (Linux only)")
+    parser.add_argument('--prev',
+                        help="timestamp of previous model to load, YYYYMMDD-HHMMSS.If not provided, train a new model")
+    parser.add_argument('--epochs', help="number of epochs to train for")
+    parser.add_argument('--data', help="path to directory containing dataset")
+    parser.add_argument('--batch', default=1,
+                        help="batch size (only 1 is supported currently due to data augmentation)")
     args = parser.parse_args()
     data_folder = "data"
     if args.data:
@@ -118,26 +160,26 @@ def main():
         TRAIN_IDS, VAL_IDS, TEST_IDS, batch_size=int(args.batch)
         )
         
-    n_classes = 6
+
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    network = Improved3DUnet(n_classes, 16, 4).to(device)
+    network = Improved3DUnet(N_CLASSES, INITIAL_UNET_CHANNELS, UNET_DEPTH).to(device)
     if args.compile:
         net_to_use = torch.compile(network)
     else:
         net_to_use = network
-    optimiser = torch.optim.Adam(network.parameters(), lr=5e-5, eps=1e-6)
+    optimiser = torch.optim.Adam(net_to_use.parameters(), lr=5e-5, eps=1e-6)
     if args.prev:
-        network.load_state_dict(torch.load(f"models/{args.prev}.model"))
+        net_to_use.load_state_dict(torch.load(f"models/{args.prev}.model"))
         optimiser.load_state_dict(torch.load(f"models/{args.prev}.optim"))
     if args.test:
-        test(network, test_loader, device)
+        test(net_to_use, test_loader, device)
     else:
         try:
             epochs = int(args.epochs)
         except:
             print("Invalid epochs specification!")
             exit()
-        train(network, optimiser, train_loader, val_loader, epochs, device, val_check_factor=1)
+        train(net_to_use, optimiser, train_loader, val_loader, epochs, device, val_check_factor=1)
 
 if __name__ == "__main__":
     main()
